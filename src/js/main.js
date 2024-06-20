065fc345048f7cfab84dd29794ab232475b728b2
Orlando <48025986+lagop@users.noreply.github.com>
2024-06-20 16:29:16 +0100

    Initial commit

---
+++ src/js/main.js
@@ -0,0 +1,61 @@
+var $item = $('.carousel-item'); 
+var $wHeight = $(window).height();
+$item.eq(0).addClass('active');
+$item.height($wHeight); 
+$item.addClass('full-screen');
+
+$('.carousel img').each(function() {
+  var $src = $(this).attr('src');
+  var $color = $(this).attr('data-color');
+  $(this).parent().css({
+    'background-image' : 'url(' + $src + ')',
+    'background-color' : $color
+  });
+  $(this).remove();
+});
+
+$(window).on('resize', function (){
+  $wHeight = $(window).height();
+  $item.height($wHeight);
+});
+
+$('.carousel').carousel({
+  interval: 6000,
+  pause: "false"
+});
+
+
+/* Portfolio */
+$(window).on('load', function() {
+    var $container = $('.portfolioContainer');
+
+    var $filter = $('#filter');
+    alert("Filter: " + $filter);
+    $container.isotope({
+        filter: '*',
+        layoutMode: 'masonry',
+        animationOptions: {
+            duration: 750,
+            easing: 'linear'
+        }
+    });
+
+    $('#filter').find('a').click(function() {
+        var selector = $(this).attr('data-filter');
+        $filter.find('a').removeClass('active');
+        $(this).addClass('active');
+        $container.isotope({
+            filter: selector,
+            layoutMode: 'masonry',
+            animationOptions: {
+                animationDuration: 750,
+                easing: 'linear',
+                queue: false,
+            }
+        });
+        return false;
+    });
+});
+
+
+  
